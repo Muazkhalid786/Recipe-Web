c3a096b1b03d7cf90e6d994c5e77969b92403038
Muaz Khalid <itzmajo786@gmail.com>
2023-08-04 20:38:46 +0500

    Store the data of regirstered user

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
 
-document.getElementById("Registeration").addEventListener('submit',function(event){
-event.preventDefault();
-console.log("Running");
-const username=document.getElementById("user-name").value;
-const useremail=document.getElementById("user-email").value;
-const userpass=document.getElementById("user-pass").value;
-
-let data = {'username': username, 'email': useremail, 'password': userpass}
-localStorage.setItem("login", JSON.stringify(data))
-console.log(data);
-console.log(username);
-    console.log( useremail);
-    console.log( userpass);
-   
+document.getElementById("Registeration").addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log("Running");
+    const username = document.getElementById("user-name").value;
+    const useremail = document.getElementById("user-email").value;
+    const userpass = document.getElementById("user-pass").value;
+    let userarr = []
+    let data = { 'username': username, 'email': useremail, 'password': userpass }
+    userarr.push(data);
+    localStorage.setItem("login", JSON.stringify(data))
+    console.log(data);
+    console.log(userarr);
+    window.location.href = "home.html";
 })
 
-
-// document.getElementById('registrationForm').addEventListener('submit', function(event) {
-//     event.preventDefault();
-  
-//     const username = document.getElementById('username').value;
-//     const email = document.getElementById('email').value;
-//     const password = document.getElementById('password').value;
-//     const confirmPassword = document.getElementById('confirmPassword').value;
-  
-//     // You can add more validation and error handling here
-  
-//     // For now, let's just display the user information in the console
-//     console.log('Username:', username);
-//     console.log('Email:', email);
-//     console.log('Password:', password);
-//     console.log('Confirm Password:', confirmPassword);
-  
-//     // You can send this data to the server for further processing
-//   });
-  
